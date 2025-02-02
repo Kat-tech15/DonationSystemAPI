feb9911d90563d68dbb94bd1788c6621a0b1e67d
Kat-tech15 <kelvinkatwai@gmail.com>
2025-02-02 23:52:29 +0300

    updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('donor', 'Donor'),
+        ('beneficiary', 'Beneficiary'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='donor')
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegissterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class ProfileView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
