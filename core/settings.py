7ed4b394a823c1cde74873e99c648fd80cc4c2ad
Kat-tech15 <kelvinkatwai@gmail.com>
2025-04-18 00:34:11 +0300

    updates

--- core/settings.py
+++ core/settings.py
@@ -126,8 +126,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'donation_db',
+        'USER': 'root',
+        'PASSWORD': '200315',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
