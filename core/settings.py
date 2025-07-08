0ded0c3ce15cb891b7e083c1829776833f3e452a
Kat-tech15 <kelvinkatwai@gmail.com>
2025-07-08 22:28:08 +0300

    modified core settings

--- core/settings.py
+++ core/settings.py
@@ -72,7 +72,7 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'dj_rest_auth.authentication.AllAuthJWTAuthentication',
+        'dj_rest_auth.authentication.AllAuthJWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
